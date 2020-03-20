c4e81ee506d40e928acb8eabe2195975422e67ee
nivstein <niv@takipi.com>
2020-03-20 13:17:19 +0200

    Implemented LogEmailService

---
+++ backend/src/main/java/org/codeorange/backend/services/email/LogEmailService.java
@@ -0,0 +1,38 @@
+package org.codeorange.backend.services.email;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.codeorange.backend.AppProperties;
+
+/*package*/ class LogEmailService extends EmailService {
+
+	private static final Logger logger = LoggerFactory.getLogger(LogEmailService.class);
+
+	public static EmailService create() {
+		return new LogEmailService();
+	}
+
+	private LogEmailService() {
+
+	}
+
+	@Override
+	public void sendEmail(String from, String to, String subject, String body) {
+
+		logger.info("+-----------------------------------------------------------------+");
+		logger.info("| From:    {}", from);
+		logger.info("| To:      {}", to);
+		logger.info("| Subject: {}", subject);
+		logger.info("+-----------------------------------------------------------------+");
+
+		String[] lines = body.split("\\r?\\n");
+
+		for (String line : lines) {
+			logger.info("| {}", line);
+		}
+
+		logger.info("+-----------------------------------------------------------------+");
+	}
+
+}
