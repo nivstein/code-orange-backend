00f1837a3e80a8405d8e7ca832ee436750506965
nivstein <niv@takipi.com>
2020-03-20 13:37:42 +0200

    Allow configurable email services

--- backend/src/main/java/org/codeorange/backend/services/email/EmailService.java
+++ backend/src/main/java/org/codeorange/backend/services/email/EmailService.java
@@ -1,7 +1,14 @@
 package org.codeorange.backend.services.email;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.codeorange.backend.AppProperties;
+
 public abstract class EmailService {
 
+	private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
+	
 	private static volatile EmailService instance = null;
 
 	public static EmailService get() {
@@ -17,9 +24,30 @@ public abstract class EmailService {
 	}
 
 	private static EmailService create() {
-		return SESEmailService.create();
+		return Type.valueOf(AppProperties.get().get("code-orange.email.service")).create();
 	}
 
 	public abstract void sendEmail(String from, String to, String subject, String body);
+
+	private static enum Type {
+
+		SES(SESEmailService.class),
+		LOG(LogEmailService.class);
+
+		private final Class<? extends EmailService> clazz;
+
+		Type(Class<? extends EmailService> clazz) {
+			this.clazz = clazz;
+		}
+
+		public EmailService create() {
+			try {
+				return (EmailService)clazz.getMethod("create").invoke(null);
+			} catch (Exception e) {
+				logger.error("Error instantiating Email service {}.", this, e);
+				return null;
+			}
+		}
+	}
 
 }
--- backend/src/main/java/org/codeorange/backend/services/email/LogEmailService.java
+++ backend/src/main/java/org/codeorange/backend/services/email/LogEmailService.java
@@ -3,8 +3,6 @@ package org.codeorange.backend.services.email;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.codeorange.backend.AppProperties;
-
 /*package*/ class LogEmailService extends EmailService {
 
 	private static final Logger logger = LoggerFactory.getLogger(LogEmailService.class);
